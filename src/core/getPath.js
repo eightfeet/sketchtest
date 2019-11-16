02e0918f3ad81b72a2cbeb0b8b584fa508f55de8
eightfeet <bajiao000000@163.com>
2019-11-16 18:27:04 +0800

    update

--- src/core/getPath.js
+++ src/core/getPath.js
@@ -1,7 +1,7 @@
 export default function(source, projectname){
-	let modelsIndex = 'https://dawenximodel.github.io/sketch/assets/modelsIndex.json';
-	let models = 'https://dawenximodel.github.io/sketch/assets/models.json';
-	let imageUrl = 'https://dawenximodel.github.io/sketch/assets/models';
+	let modelsIndex = 'https://dawenximodel.github.io/sm1/assets/modelsIndex.json';
+	let models = 'https://dawenximodel.github.io/sm1/assets/models.json';
+	let imageUrl = 'https://dawenximodel.github.io/sm1/assets/models';
 	switch (`${source}-${projectname}`) {
 		case 'value':
 			break;
